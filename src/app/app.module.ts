30003b6c150c9bdc3200221608b55f31db4a0e88
sanjay <saanjaaykumaar@gmail.com>
2017-12-17 17:39:27 +0530

    image layout fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,18 @@ import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { AdmissionsComponent } from './admissions/admissions.component';
+import { FacultyComponent } from './faculty/faculty.component';
+import { ProgramsComponent } from './programs/programs.component';
+import { ServicesComponent } from './services/services.component';
 
 const appRoutes: Routes = [
-   {path: '', redirectTo: '/home', pathMatch: 'full'},
-   {path: 'home', component: HomeComponent},
-   {path: 'about', component: AboutComponent}
+   {path: '', component: HomeComponent, pathMatch: 'full'},
+   {path: 'about', component: AboutComponent},
+   {path: 'admission', component: AdmissionsComponent},
+   {path: 'faculty', component: FacultyComponent},
+   {path: 'programs', component: ProgramsComponent},
+   {path: 'services', component: ServicesComponent}
 ];
 
 
@@ -28,7 +35,11 @@ const appRoutes: Routes = [
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    AdmissionsComponent,
+    FacultyComponent,
+    ProgramsComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
